2c6dd4e78f51daa8f9e84433b24d6a2f49c84b7e
Kevintonn <natraps300@gmail.com>
2017-04-15 17:57:20 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,14 +2,45 @@ var api = "http://api.giphy.com/v1/gifs/search?";
 var apiKey = "&api_key=dc6zaTOxFJmzC";
 var query = "&q=dog";
 
+
+var rotY = 0;
+var wMap
+var bLight
+
+function preload() {
+    wMap = loadImage("http://i.imgur.com/9a8qyPh.jpg");
+    bLight = loadImage("http://i.imgur.com/sJSN8Q2.jpg");
+}
+
 function setup() {
-    createCanvas(window.innerWidth, window.innerHeight);
+    createCanvas(window.innerWidth, window.innerHeight, WEBGL);
     var url = api + apiKey + query;
     loadJSON(url, gotData);
 
 }
 
 function gotData(giphy) {
-    createImg(giphy.data[0].images.original.url);
+     tMap = createImg(giphy.data[0].images.original.url);
+  
+}
+
+function draw() {
+    background(0);
+    
+    var m = map(mouseY, 0, window.innerWidth, 0, 10)/300;
+    var mA = map(mouseX, 0, window.innerWidth, 0, 30);
+    
+    push();
+    translate(35-mA, -35, -150);
+    texture(bLight);
+    box(400);
+    pop();
+    
+    translate(0+mA, 0, 0);
+    rotateY(rotY);
+    texture(tMap);
+    sphere(100);
+    
+    rotY += 0+m;
   
 }
